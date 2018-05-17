e50431d8ab97432e55258086db37e59b1ac40944
s-xq <971070045@qq.com>
2018-05-17 10:42:46 +0800

    add library apollographql to generate model class; add github schema.json

--- app/src/main/java/com/sxq/github/MainActivity.java
+++ app/src/main/java/com/sxq/github/MainActivity.java
@@ -3,40 +3,11 @@ package com.sxq.github;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import graphql.ExecutionResult;
-import graphql.GraphQL;
-import graphql.schema.GraphQLSchema;
-import graphql.schema.StaticDataFetcher;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
-
-import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String schema = "type Query{hello: String}";
-
-        SchemaParser schemaParser = new SchemaParser();
-        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
-
-        RuntimeWiring runtimeWiring = newRuntimeWiring()
-                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
-                .build();
-
-        SchemaGenerator schemaGenerator = new SchemaGenerator();
-        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
-
-        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
-        ExecutionResult executionResult = build.execute("{hello}");
-
-        System.out.println(executionResult.getData().toString());
-        // Prints: {hello=world}
     }
 }
---
+++ docs/step.md
@@ -0,0 +1,5 @@
+## Download Github schema.json
+* [apollo-codegen](https://www.apollographql.com/docs/ios/downloading-schema.html)
+```sh
+apollo-codegen download-schema https://api.github.com/graphql --output schema.json --header "Authorization: Bearer <token>"
+```
