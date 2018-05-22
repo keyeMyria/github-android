89db2c16f4b48d778c4df60fcbc2c43fd63b0c23
s-xq <971070045@qq.com>
2018-05-22 21:38:05 +0800

    add emoji.json ; show toolbar

--- app/src/main/java/com/sxq/github/MainActivity.java
+++ app/src/main/java/com/sxq/github/MainActivity.java
@@ -4,8 +4,7 @@ import android.os.Bundle;
 
 import com.sxq.github.data.model.login.Login;
 import com.sxq.github.ui.base.BaseActivity;
-import com.sxq.github.ui.modules.profile.followers.ProfileFollowerFragment;
-import com.sxq.github.utils.ActivityUtil;
+import com.sxq.github.ui.modules.user.UserActivity;
 
 /**
  * Created by shixiaoqiang01 on 2018/5/10.
@@ -22,11 +21,6 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ProfileFollowerFragment profileFollowerFragment =
-                (ProfileFollowerFragment)getSupportFragmentManager().findFragmentById(R.id.contentFrame);
-        if(profileFollowerFragment==null){
-            profileFollowerFragment = ProfileFollowerFragment.newInstance(Login.getCurrentUser().getLogin());
-            ActivityUtil.addFragmentToActivity(getSupportFragmentManager(), profileFollowerFragment, R.id.contentFrame);
-        }
+        UserActivity.startActivity(this, Login.getCurrentUser().getLogin());
     }
 }
