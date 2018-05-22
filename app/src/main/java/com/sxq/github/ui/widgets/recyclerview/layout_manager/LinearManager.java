dd5dedce3b7983f4b6501515944a9b019642fe2f
s-xq <971070045@qq.com>
2018-05-22 23:03:13 +0800

    add custom LinearManager

---
+++ app/src/main/java/com/sxq/github/ui/widgets/recyclerview/layout_manager/LinearManager.java
@@ -0,0 +1,36 @@
+package com.sxq.github.ui.widgets.recyclerview.layout_manager;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+public class LinearManager extends LinearLayoutManager {
+
+    public LinearManager(Context context) {
+        super(context);
+    }
+
+    public LinearManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    public LinearManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (Exception ignored) {}
+    }
+
+    @Override
+    public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state, int widthSpec, int heightSpec) {
+        try {
+            super.onMeasure(recycler, state, widthSpec, heightSpec);
+        } catch (Exception ignored) {}
+    }
+
+}
